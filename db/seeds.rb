be005d1efc2817b8a51825472adc5ec9b1c4f1ea
Luke Weaver <lukeweaver@Lukes-MBP.home>
2017-07-21 07:55:28 -0400

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eros magna, consectetur nec sapien sit amet, laoreet semper felis. Nam et auctor purus, ac pharetra dolor. Morbi cursus turpis sem, eget elementum ante tempor a. Cras molestie tortor ut nunc porttitor accumsan. Aenean nec venenatis purus. Vivamus et viverra lectus, nec facilisis metus. Praesent facilisis vehicula magna vel bibendum. Nam eu sodales enim. Aenean sodales lacus quis enim eleifend, sed ultricies magna lobortis. Proin finibus, ipsum et molestie egestas, lacus eros lacinia ipsum, molestie finibus tellus sem ut lectus. Vivamus neque felis, dignissim sit amet cursus in, consequat vel metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec nec aliquam dolor. Sed metus sapien, eleifend sit amet euismod gravida, venenatis ac nibh. Fusce ex neque, venenatis tempor accumsan eget, volutpat a mauris. Nullam bibendum massa vitae ligula mattis, ac posuere sapien lacinia."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+  title: "Portfolio title: #{portfolio_item}",
+  subtitle: "My great service",
+  body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eros magna, consectetur nec sapien sit amet, laoreet semper felis. Nam et auctor purus, ac pharetra dolor. Morbi cursus turpis sem, eget elementum ante tempor a. Cras molestie tortor ut nunc porttitor accumsan. Aenean nec venenatis purus. Vivamus et viverra lectus, nec facilisis metus. Praesent facilisis vehicula magna vel bibendum. Nam eu sodales enim. Aenean sodales lacus quis enim eleifend, sed ultricies magna lobortis. Proin finibus, ipsum et molestie egestas, lacus eros lacinia ipsum, molestie finibus tellus sem ut lectus. Vivamus neque felis, dignissim sit amet cursus in, consequat vel metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec nec aliquam dolor. Sed metus sapien, eleifend sit amet euismod gravida, venenatis ac nibh. Fusce ex neque, venenatis tempor accumsan eget, volutpat a mauris. Nullam bibendum massa vitae ligula mattis, ac posuere sapien lacinia.",
+  main_image: "http://via.placeholder.com/350x150",
+  thumb_image: "http://via.placeholder.com/350x150"
+)
+end
+
+puts "9 portfolio items created"
